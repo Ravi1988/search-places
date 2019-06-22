5f03ad88b4308b9db14af44e5f46f19ecd0f1ea7
Ravi Kumar <erravisec@gmail.com>
2019-06-22 15:56:16 +0530

    app module will present the presenter layer

--- app/src/main/java/com/app/searchplaces/ui/MainActivity.java
+++ app/src/main/java/com/app/searchplaces/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.app.searchplaces;
+package com.app.searchplaces.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.app.presentation.R;
 
 public class MainActivity extends AppCompatActivity {
 
